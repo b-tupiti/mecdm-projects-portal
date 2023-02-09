26c422471072f95aa7260bd6b6215b1fcf8c1cf0
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-09 14:47:47 +1100

    created Projects view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import  login_required
+from users.utils import isAdminUser
+from django.shortcuts import redirect
 
-# Create your views here.
+@login_required(login_url='login')
+def Projects(request):
+    
+    if not isAdminUser(request):
+        return redirect('home')
+    
+    context = {}
+    
+    return render(request,'projects/projects.html', context)
