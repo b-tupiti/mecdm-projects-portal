601b089c91f136efb844900141a9ea1a2a2de0d4
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-09 14:44:08 +1100

    included urls for projects app

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('requests/', include('requests.urls')),
+    path('projects/', include('projects.urls')),
     
     # temporary
     path('', home, name="home"),
