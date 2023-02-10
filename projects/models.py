a192bb699776f7b697fcc2a0997c9b00f257f84d
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-10 20:30:58 +1100

    implemented MTV for all projects

--- projects/models.py
+++ projects/models.py
@@ -6,8 +6,8 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True,null=True)
     donors = models.ManyToManyField(Donor, verbose_name="Donor Agencies",  blank=True)
-    implementors = models.ManyToManyField(Partner, verbose_name="Implementing Agencies", blank=True)
-    partners = models.ManyToManyField(Implementor, verbose_name="Accreditted Entities\\Partner Organizations",  blank=True)
+    implementors = models.ManyToManyField(Implementor, verbose_name="Implementing Agencies", blank=True)
+    partners = models.ManyToManyField(Partner, verbose_name="Accreditted Entities\\Partner Organizations",  blank=True)
     
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import  login_required
 from users.utils import isAdminUser
 from django.shortcuts import redirect
+from requests.models import AccountRequest
+from .models import Project
+from entities.models import *
 
-@login_required(login_url='login')
 def Projects(request):
     
-    if not isAdminUser(request):
-        return redirect('home')
+    is_admin = isAdminUser(request)
+    total_requests = AccountRequest.objects.count()
     
-    context = {}
+    projects = Project.objects.all()
+   
+    search_filters = {
+        'donors' :  Donor.objects.all(),
+        'implementors': Implementor.objects.all(),
+        'partners': Partner.objects.all(),
+    }
+    
+    context = {
+        'is_admin':is_admin,
+        'total_requests':total_requests,
+        'projects': projects,
+        'search_filters' : search_filters,
+    }
     
     return render(request,'projects/projects.html', context)
