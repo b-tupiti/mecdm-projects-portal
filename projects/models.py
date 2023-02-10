246ad31e95edfb5d4a8ba85e4c396e30e2016fd3
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-10 14:44:28 +1100

    added entities as many-to-many fields in project

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from entities.models import *
+import uuid
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True,null=True)
+    donors = models.ManyToManyField(Donor, verbose_name="Donor Agencies",  blank=True)
+    implementors = models.ManyToManyField(Partner, verbose_name="Implementing Agencies", blank=True)
+    partners = models.ManyToManyField(Implementor, verbose_name="Accreditted Entities\\Partner Organizations",  blank=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    
