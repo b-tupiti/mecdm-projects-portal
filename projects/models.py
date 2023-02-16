aac0d1078f941890b4c6fead43c87d83f4a5401e
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-16 12:12:03 +1100

    added Scope and Location to projects app

--- projects/models.py
+++ projects/models.py
@@ -10,10 +10,12 @@ class Project(models.Model):
     risk_rate = models.ForeignKey('Risk', null=True, blank=True, on_delete=models.SET_NULL)
     type = models.ForeignKey('ProjectType', null=True, blank=True, on_delete=models.SET_NULL)
     status = models.ForeignKey('Status', null=True, blank=True, on_delete=models.SET_NULL)
+    scope = models.ForeignKey('Scope', null=True, blank=True, on_delete=models.SET_NULL)
     
     donors = models.ManyToManyField(Donor, verbose_name="Donor Agencies",  blank=True)
     implementors = models.ManyToManyField(Implementor, verbose_name="Implementing Agencies", blank=True)
     partners = models.ManyToManyField(Partner, verbose_name="Accreditted Entities\\Partner Organizations",  blank=True)
+    locations = models.ManyToManyField('Location', verbose_name="Project Locations",  blank=True)
     tags = models.ManyToManyField("Tag", blank=True)
     
     created = models.DateTimeField(auto_now_add=True)
@@ -91,4 +93,30 @@ class Tag(models.Model):
         return self.name
     
     
+class Scope(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
+    def natural_key(self):
+        return self.name 
+    
+    def __str__(self):
+        return self.name
+    
+
+class Location(models.Model):
+    name = models.CharField(max_length=400, unique=True)
+    lattitude = models.FloatField(blank=True,null=True)
+    longitude = models.FloatField(blank=True,null=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def natural_key(self):
+        return self.name 
+    
+    def __str__(self):
+        return self.name
