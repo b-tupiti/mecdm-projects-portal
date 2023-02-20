94fd40b656e364bdfbc0c8c0cc9b35acbefd6098
Brandon Tupiti <tupitibrandon@gmail.com>
2023-02-21 10:55:06 +1100

    edited settings.py

--- config/settings.py
+++ config/settings.py
@@ -141,8 +141,7 @@ MEDIA_URL = '/uploads/'
 # Path where media is stored
 MEDIA_ROOT = BASE_DIR / 'uploads/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
